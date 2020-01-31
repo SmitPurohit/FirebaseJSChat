2c3d3f3bdd5ec962ab9cb8b8be37d0d3642bea01
SmitPurohit <35670650+SmitPurohit@users.noreply.github.com>
2020-01-30 21:19:27 -0600

    Adds custom room keys
    Now users can connect to any room key, allowing private rooms to be created

---
+++ config.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var config = {
+    apiKey: "AIzaSyBayLFfDurA0OkvPu7Gksk3ywol0JjFPgQ",
+    authDomain: "presidential-29357.firebaseapp.com",
+    databaseURL: "https://presidential-29357.firebaseio.com",
+    projectId: "presidential-29357",
+    storageBucket: "presidential-29357.appspot.com",
+    messagingSenderId: "521503005949",
+    appId: "1:521503005949:web:1e9ca494fcb74aef423294",
+    measurementId: "G-VKCEWDLV4Z"
+
+};
